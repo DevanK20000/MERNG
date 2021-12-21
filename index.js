01d125b6aff2b95fea741089cdb0bac9f813bd1e
Devan Khandagale <devankhandagale@gmail.com>
2021-12-21 10:38:34 +0530

    port

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { ApolloServer } = require("apollo-server");
 const { PubSub } = require("graphql-subscriptions");
 // const gql = require("graphql-tag");
 const mongoose = require("mongoose");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
@@ -9,10 +12,12 @@ const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
 
 const pubsub = new PubSub();
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   context: ({ req }) => ({ req, pubsub }),
 });
 
@@ -25,8 +30,11 @@ mongoose
   })
   .then(() => {
     console.log("MongoDB Connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.log(err);
   });
